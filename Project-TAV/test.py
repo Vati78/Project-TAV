21ca39e36b324884f540092b0d7909cf4e73cd9c
leika123 <legal.aurelien@live.fr>
2025-05-15 19:47:40 +0200

    plus de retour a la ligne pour le nombre

--- Project-TAV/test.py
+++ Project-TAV/test.py
@@ -5,8 +5,9 @@ Created on Thu May 15 17:25:32 2025
 @author: collier2
 """
 for i in range(100):
-    print("ceci est un petit test\n{}".format(i))
+    print("ceci est un petit test {}".format(i))
 print(1+1)
 print("test")
 #test 
 # QU EST CE QUE VOUS VOULEZ FAIRE !!!!!!!!! DITES LE !!!!
+#test de merge 
