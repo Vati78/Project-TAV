8fd4b8a50fffb90fb6627234bc769bd58e2284a6
Tim_pc1 <you@example.com>
2025-05-19 19:23:03 +0200

    Test parce que j'aime bien tester

--- Project-TAV/test.py
+++ Project-TAV/test.py
@@ -6,7 +6,7 @@ Created on Thu May 15 17:25:32 2025
 """
 for i in range(100):
     print("ceci est un petit test {}".format(i))
-print(1+1)
+print(1+1+1)
 print("test")
 #test 
 # QU EST CE QUE VOUS VOULEZ FAIRE !!!!!!!!! DITES LE !!!!
