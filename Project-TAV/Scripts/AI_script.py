d4604e90aa9396f9d75096293fcbf307abc07667
Vati78 <val.gte78@gmail.com>
2025-05-22 16:43:18 +0200

    ai_script

---
+++ Project-TAV/Scripts/AI_script.py
@@ -0,0 +1,13 @@
+# liens cours :
+#   - https://www.tensorflow.org/guide/tensor?hl=fr
+#features = input
+#label = output
+
+#Types of machine learning :
+#   - Supervised machine learning : features+labels, compares prediction with label
+#   - Unsupervised machine learning : only features, makes prediction
+#   - Reinforcement machine learning : testing with rewards
+
+import tensorflow as tf
+
+
