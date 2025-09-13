f556fc91690be87cf2fb0cc6e2bfc2848f679b60
Vati78 <val.gte78@gmail.com>
2025-09-13 15:53:43 +0200

    modified game.py -- fixed rebounce

--- game.py
+++ game.py
@@ -19,6 +19,7 @@ dGREEN = (88,120,56)
 WHITE = (238,238,210)
 dWHITE = (208,208,180)
 GREY = (50, 50, 50)
+RED = (255, 20, 20)
 
 win.fill(GREY)
 
@@ -31,9 +32,10 @@ class Ball:
         self.radius = radius
         self.color = color
 
-    def rebond(self, plateforme):
-        self.vx = -self.vx
-        self.vy = -self.vy + plateforme.vy
+    def rebond(self, coeff, touche_exterieur):
+        bord = -1 if touche_exterieur else 1
+        self.vx = - (self.vx + coeff) * bord
+        self.vy = - (self.vy + coeff)
 
     def draw(self):
         pg.draw.circle(win, self.color, (self.x, self.y), self.radius)
@@ -41,6 +43,7 @@ class Ball:
     def move(self):
         self.x += self.vx
         self.y += self.vy
+
 
 class Plateform:
     def __init__(self, x, y, lx, ly, ymax, ymin, color):
@@ -51,12 +54,12 @@ class Plateform:
         self.vy = 0
         self.ymax = ymax
         self.ymin = ymin
-        self.vmax = 4
+        self.vmax = 10
         self.color = color
 
     def move(self, vy):
         vy *= self.vmax
-        if self.ymin < self.y + vy< self.ymax:
+        if self.ymin < self.y + vy < self.y + vy + self.ly < self.ymax:
             self.y += vy
             self.vy = vy
 
@@ -69,12 +72,11 @@ class Plateform:
 
 def main():
 
-    players = [Plateform(10, HEIGHT//2, 10, 100, WIDTH-40, 0, GREEN), Plateform(WIDTH-20, HEIGHT//2, 10, 100, WIDTH-40, 0, GREEN)]
-    balle = Ball(WIDTH//2, HEIGHT//2, 10, 0, 40, GREEN)
+    players = [Plateform(10, HEIGHT//2, 10, 100, HEIGHT, 0, GREEN), Plateform(WIDTH-20, HEIGHT//2+10, 10, 100, HEIGHT, 0, GREEN)]
+    balle = Ball(WIDTH//2, HEIGHT//2 - 10, 10, 0, 40, RED)
     running = True
 
     while running:
-        keys = pg.key.get_pressed()
         for i in players: i.reset()
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -89,20 +91,34 @@ def main():
 
         balle.move()
 
-        if players[0].y <= balle.y <= players[0].y + players[0].ly:
-            if balle.x - balle.radius <= players[0].x + players[0].lx and balle.vx < 0:
-                balle.rebond(players[0])
-        elif players[0].y - balle.radius <= balle.y <= players[0].y:
-            if math.sqrt((balle.y - players[0].y)^2 + (balle.x - (players[0].x + players[0].lx))^2) <= balle.radius:
-                balle.rebond(players[0])
-        elif players[0].y + players[0].ly<= balle.y <= players[0].y + players[0].ly + balle.radius :
-            if ((balle.y - (players[0].y - players[0].ly))**2 + (balle.x - players[0].x + players[0].lx)**2)**0.5:
-                balle.rebond(players[0])
+        if balle.vx < 0:
+            if (players[0].y <= balle.y <= players[0].y + players[0].ly
+                    and balle.x - balle.radius <= players[0].x + players[0].lx):
+                balle.rebond(players[0].vy, False)
+            elif players[0].y - balle.radius + 1 <= balle.y <= players[0].y:
+                if math.sqrt((balle.y - players[0].y)**2 + (balle.x - (players[0].x + players[0].lx))**2) <= balle.radius:
+                    balle.rebond(players[0].vy, False)
+            elif players[0].y + players[0].ly <= balle.y <= players[0].y + players[0].ly + balle.radius:
+                if math.sqrt((balle.y - (players[0].y + players[0].ly))**2 + (balle.x - (players[0].x + players[0].lx))**2) <= balle.radius:
+                    balle.rebond(players[0].vy, False)
 
-        if (balle.x + balle.radius >= players[1].x
-                and players[1].y <= balle.y <= players[1].y + players[1].ly)\
-                and balle.vx > 0:
-            balle.rebond(players[1])
+        if balle.vx > 0:
+            if (players[1].y <= balle.y <= players[1].y + players[1].ly
+                    and balle.x + balle.radius >= players[1].x):
+                balle.rebond(players[1].vy, False)
+            elif players[1].y - balle.radius + 1 <= balle.y <= players[1].y:
+                if math.sqrt((balle.y - players[1].y)**2 + (balle.x - players[1].x)**2) <= balle.radius:
+                    balle.rebond(players[1].vy, False)
+            elif players[1].y + players[1].ly <= balle.y <= players[1].y + players[1].ly + balle.radius:
+                if math.sqrt((balle.y - (players[1].y + players[1].ly))**2 + (balle.x - players[1].x)**2) <= balle.radius:
+                    balle.rebond(players[1].vy, False)
+
+        if balle.y - balle.radius <= 0:
+            balle.rebond(0, True)
+        elif balle.y + balle.radius >= HEIGHT:
+            balle.rebond(0, True)
+
+
 
         #display
         win.fill(GREY)
