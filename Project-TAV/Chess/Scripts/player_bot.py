99068f03def37dcc5ee814b59c57752db3f1c5c8
Vati78 <val.gte78@gmail.com>
2026-09-18 14:38:23 +0200

    fixed bot bug

--- Project-TAV/Chess/Scripts/player_bot.py
+++ Project-TAV/Chess/Scripts/player_bot.py
@@ -78,7 +78,7 @@ class Bot(Item):
         super().__init__(color, timer)
         self.calculated_move = None
         self.bot_calculating = False
-        self.depth = 3
+        self.depth = 4
 
         self.virtual_environment = virtual_environment
 
@@ -87,7 +87,11 @@ class Bot(Item):
 
         status = gestionary.chess_game.status_to_key()
         self.virtual_environment.chess_game.key_to_status(status)
-        self.virtual_environment.chess_game.list_position.append(status)
+
+        self.virtual_environment.chess_game.player_turn = int(self.color == "b")
+        self.virtual_environment.chess_game.index_position = 0
+
+        self.virtual_environment.chess_game.list_position = [status]
 
         th.Thread(target=self.minimax_with_alpha_beta_pruning,
                   args=(self.depth, -math.inf, math.inf, int(self.color == "b"), self.depth)).start()
@@ -133,7 +137,7 @@ class Bot(Item):
                                 undo_move()
                                 #print(2*depth*"_", "Evaluation :", child_eval)
                                 if max_eval == child_eval or best_move == []: best_move.append((i, move, promoted_piece))
-                                elif max_eval > child_eval: best_move = [(i, move, promoted_piece)]
+                                elif max_eval < child_eval: best_move = [(i, move, promoted_piece)]
                                 max_eval = max(child_eval, max_eval)
                                 alpha = max(child_eval, alpha)
                                 if alpha >= beta:
